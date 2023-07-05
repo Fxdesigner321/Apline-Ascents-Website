4b6d0a880b85f0c26984a91f7b3386fdf1612988
Saif khan <106547546+Fxdesigner321@users.noreply.github.com>
2023-07-05 16:31:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var data=0;
+document.getElementById("root1").innerText=data;
+function decrement1(){
+    data=data-1;
+    document.getElementById("root1").innerText=data;
+}
+function increment1(){
+    data=data+1
+    document.getElementById("root1").innerHTML=data;
+}
+var data=0;
+document.getElementById("root2").innerText=data;
+function decrement2(){
+    data=data-1;
+    document.getElementById("root2").innerText=data;
+}
+function increment2(){
+    data=data+1
+    document.getElementById("root2").innerHTML=data;
+}
+var data=0;
+document.getElementById("root3").innerText=data;
+function decrement3(){
+    data=data-1;
+    document.getElementById("root3").innerText=data;
+}
+function increment3(){
+    data=data+1
+    document.getElementById("root3").innerHTML=data;
+}
